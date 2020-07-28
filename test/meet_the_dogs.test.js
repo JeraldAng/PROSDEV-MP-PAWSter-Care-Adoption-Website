4c7afa97c887bceaea7167e46481eb7f336bebe4
JeraldAng <43471154+JeraldAng@users.noreply.github.com>
2020-07-28 22:54:18 +0800

    added 7 search bar test cases
    added 7 search bar test cases

--- test/meet_the_dogs.test.js
+++ test/meet_the_dogs.test.js
@@ -41,6 +41,132 @@ describe('Filter search results', () => {
         '</div>' + 
         '<span data-name = "Benny" data-breed = "Beagle" data-gender = "male" data-energy_level = "high" data-ease_of_training = "easy" class="product">'; 
     
+    test('search: keyword not found', () => {
+	  // Arrange
+      $('#search-box').val("random");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeFalsy();
+        expect($("#Harp").css('display')).toBeFalsy();
+        done()
+      }, 200)    
+      
+    })
+    
+    test('search: name found', () => {
+	  // Arrange
+      $('#search-box').val("Benny");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeTruthy();
+        expect($("#Harp").css('display')).toBeFalsy();
+        done()
+      }, 200)    
+      
+    })
+    
+    test('search: breed found', () => {
+	  // Arrange
+      $('#search-box').val("Husky");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeFalsy();
+        expect($("#Harp").css('display')).toBeTruthy();
+        done()
+      }, 200)    
+      
+    })
+    
+    test('search: gender found', () => {
+	  // Arrange
+      $('#search-box').val("Female");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeFalsy();
+        expect($("#Harp").css('display')).toBeTruthy();
+        done()
+      }, 200)    
+      
+    })
+    
+    test('search: energy level found', () => {
+	  // Arrange
+      $('#search-box').val("high");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeTruthy();
+        expect($("#Harp").css('display')).toBeFalsy();
+        done()
+      }, 200)    
+      
+    })
+    
+    test('search: ease of training found', () => {
+	  // Arrange
+      $('#search-box').val("easy");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeTruthy();
+        expect($("#Harp").css('display')).toBeFalsy();
+        done()
+      }, 200)    
+      
+    })
+    
+    test('search: check if case sensitive', () => {
+	  // Arrange
+      $('#search-box').val("HuSkY");    
+        
+	  // Act
+      dogFunc.searchFilter();
+      
+	  // Assert
+      // jQuery takes 200ms to hide an html component
+      expect(document.hidden).toEqual(false);
+      setTimeout(function() {
+        expect($("#Benny").css('display')).toBeFalsy();
+        expect($("#Harp").css('display')).toBeTruthy();
+        done()
+      }, 200)    
+      
+    })
+    
     // selected option should return to default
     test('clear search', () => {
 	  // Arrange
@@ -59,15 +185,5 @@ describe('Filter search results', () => {
       expect($(".filter-energy_level").val()).toEqual("");
       expect($(".filter-ease_of_training").val()).toEqual("");
       expect($("#search-box").val()).toEqual("");
-  })
-    
-  test('search bar filter', () => {
-	  // Arrange
-       
-	  // Act
-      //dogFunc.searchFilter();
-      
-	  // Assert
-      
   })
 });
