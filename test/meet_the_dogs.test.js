644a49b3674c81fa135df3750e192ad6d3e27a8e
JeraldAng <43471154+JeraldAng@users.noreply.github.com>
2020-08-03 20:32:20 +0800

    update signup branch
    update signup branch

--- test/meet_the_dogs.test.js
+++ test/meet_the_dogs.test.js
@@ -1,17 +1,13 @@
-// import jQuery to test so that it will recognize jQuery commands
-global.jQuery = require('../public/js/jquery');
-global.$ = global.jQuery;
+const $ = require('../public/js/jquery');
+const { clearFilter } = require('../public/js/meet_the_dogs');
 
-// dogFunc will contain all exported functions 
-const dogFunc = require('../public/js/meet_the_dogs');
+jest.mock('../public/js/meet_the_dogs');
 
 describe('Filter search results', () => {
-    
-    // simulate the html, since majority of the functions rely on changing html properties
-    // use this for the entire test
-    document.body.innerHTML =
-        '<form id="search-form" action="" method="POST" enctype="multipart/form-data">' +
-        '<input id="search-box" value=""/>' +
+  test('clear search', () => {
+	  // Arrange
+	  document.body.innerHTML =
+        '<input id="search-box" value="search"/>' +
         '<button id = "reset-filter"/>' +
         '<div>' +
         '<div>' +
@@ -38,148 +34,17 @@ describe('Filter search results', () => {
         '<option value="easy">Easy</option>' +
         '</select>' +
         '</div>' +
-        '</div>' + 
-        '<span data-name = "Benny" data-breed = "Beagle" data-gender = "male" data-energy_level = "high" data-ease_of_training = "easy" class="product">'; 
-    
-    test('search: keyword not found', () => {
-	  // Arrange
-      $('#search-box').val("random");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeFalsy();
-        expect($("#Harp").css('display')).toBeFalsy();
-        done()
-      }, 200)    
-      
-    })
-    
-    test('search: name found', () => {
-	  // Arrange
-      $('#search-box').val("Benny");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeTruthy();
-        expect($("#Harp").css('display')).toBeFalsy();
-        done()
-      }, 200)    
-      
-    })
-    
-    test('search: breed found', () => {
-	  // Arrange
-      $('#search-box').val("Husky");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeFalsy();
-        expect($("#Harp").css('display')).toBeTruthy();
-        done()
-      }, 200)    
-      
-    })
-    
-    test('search: gender found', () => {
-	  // Arrange
-      $('#search-box').val("Female");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeFalsy();
-        expect($("#Harp").css('display')).toBeTruthy();
-        done()
-      }, 200)    
-      
-    })
-    
-    test('search: energy level found', () => {
-	  // Arrange
-      $('#search-box').val("high");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeTruthy();
-        expect($("#Harp").css('display')).toBeFalsy();
-        done()
-      }, 200)    
-      
-    })
-    
-    test('search: ease of training found', () => {
-	  // Arrange
-      $('#search-box').val("easy");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeTruthy();
-        expect($("#Harp").css('display')).toBeFalsy();
-        done()
-      }, 200)    
-      
-    })
-    
-    test('search: check if case sensitive', () => {
-	  // Arrange
-      $('#search-box').val("HuSkY");    
-        
-	  // Act
-      dogFunc.searchFilter();
-      
-	  // Assert
-      // jQuery takes 200ms to hide an html component
-      expect(document.hidden).toEqual(false);
-      setTimeout(function() {
-        expect($("#Benny").css('display')).toBeFalsy();
-        expect($("#Harp").css('display')).toBeTruthy();
-        done()
-      }, 200)    
-      
-    })
-    
-    // selected option should return to default
-    test('clear search', () => {
-	  // Arrange
-      $('.filter-breed').val("Beagle");
-      $('.filter-gender').val("male");
-      $('.filter-energy_level').val("high");
-      $('.filter-ease_of_training').val("easy");
-      $('#search-box').val("search for dogs");
+        '</div>'; 
+      $(".filter-breed").val("Beagle").change();
+      $(".filter-gender").val("male").change();
+      $(".filter-energy_level").val("high").change();
+      $(".filter-ease_of_training").val("easy").change();
 
 	  // Act
-      dogFunc.clearFilter();
+      $("#reset-filter").click();
       
 	  // Assert
+	  expect(clearFilter).toBeCalled();
       expect($(".filter-breed").val()).toEqual("");
       expect($(".filter-gender").val()).toEqual("");
       expect($(".filter-energy_level").val()).toEqual("");
