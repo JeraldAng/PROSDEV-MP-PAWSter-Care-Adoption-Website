536710911666137d098d0af2733cec4be3397317
Jeremy Dale Coronia <jeremy_coronia@dlsu.edu.ph>
2020-08-04 22:52:55 +0800

    updated input validation

--- public/js/requestform.js
+++ public/js/requestform.js
@@ -28,14 +28,8 @@ $(document).ready(function(){
   var reqFirst = document.getElementById("reqFirst");
   var reqLast = document.getElementById("reqLast");
   var email = document.getElementById("reqEmail");
-  var phone = document.getElementById("reqNum");
-  var ph = new RegExp(/^(09|\+639)\d{9}$/);
-
-  phone.onkeyup = function(){
-      if (!ph.test(phone)){
-        $('#invalid-phone').html("Please choose a valid phone number.").css('color', 'red');  
-    }
-  }
+  // var phone = document.getElementById("reqNum");
+  // var ph = new RegExp(/^(09|\+639)\d{9}$/);
 
   email.onkeyup = function(){
       if(!email.checkValidity()){
@@ -74,17 +68,15 @@ $(document).ready(function(){
 (function() {
       'use strict';
       window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-      if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-      }
-      form.classList.add('was-validated');
-      }, false);
-      });
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+        });
       }, false);
 })();
