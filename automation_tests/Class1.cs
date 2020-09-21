853cfd0d7378f04e66b44632146f3e151914d273
siafrancine <47266577+siafrancine@users.noreply.github.com>
2020-09-21 13:12:02 +0800

    Francine Setup

---
+++ automation_tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace PROSDEV_MP
+{
+    public class Class1
+    {
+        [Fact]
+
+        public void FirstTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
